98e40121556e9dedd254db303df17c2053debbc2
GABO1017 <gabrielfranco1017@gmail.com>
2024-10-27 20:19:51 -0500

    HU10 Validaciones en frontend y backend para los formularios de registro y login.

---
+++ src/components/UI/Loading.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import logo1 from "../../assets/images/MuiscaIcons/logo1.png";
+import logo2 from "../../assets/images/MuiscaIcons/logo2.png";
+import logo3 from "../../assets/images/MuiscaIcons/logo3.png";
+import logo4 from "../../assets/images/MuiscaIcons/logo4.png";
+import logo5 from "../../assets/images/MuiscaIcons/logo5.png";
+import logo6 from "../../assets/images/MuiscaIcons/logo6.png";
+import logo7 from "../../assets/images/MuiscaIcons/logo7.png";
+import logo8 from "../../assets/images/MuiscaIcons/logo8.png";
+import logo9 from "../../assets/images/MuiscaIcons/logo9.png";
+import logo10 from "../../assets/images/MuiscaIcons/logo10.png";
+import logo11 from "../../assets/images/MuiscaIcons/logo11.png";
+import logo12 from "../../assets/images/MuiscaIcons/logo12.png";
+import logo13 from "../../assets/images/MuiscaIcons/logo13.png";
+import logo14 from "../../assets/images/MuiscaIcons/logo14.png";
+import logo15 from "../../assets/images/MuiscaIcons/logo15.png";
+import logo16 from "../../assets/images/MuiscaIcons/logo16.png";
+import logo17 from "../../assets/images/MuiscaIcons/logo17.png";
+import logo18 from "../../assets/images/MuiscaIcons/logo18.png";
+import logo19 from "../../assets/images/MuiscaIcons/logo19.png";
+import logo20 from "../../assets/images/MuiscaIcons/logo20.png";
+import logo21 from "../../assets/images/MuiscaIcons/logo21.png";
+import logo22 from "../../assets/images/MuiscaIcons/logo22.png";
+import logo23 from "../../assets/images/MuiscaIcons/logo23.png";
+import logo24 from "../../assets/images/MuiscaIcons/logo24.png";
+import logo25 from "../../assets/images/MuiscaIcons/logo25.png";
+import logo26 from "../../assets/images/MuiscaIcons/logo26.png";
+import "../../styles/global.css";
+
+const Loading = () => {
+  // Lista de iconos (URLs o paths de los íconos que quieres usar)
+  const icons = [
+    logo1,
+    logo2,
+    logo3,
+    logo4,
+    logo5,
+    logo6,
+    logo7,
+    logo8,
+    logo9,
+    logo10,
+    logo11,
+    logo12,
+    logo13,
+    logo14,
+    logo15,
+    logo16,
+    logo17,
+    logo18,
+    logo19,
+    logo20,
+    logo21,
+    logo22,
+    logo23,
+    logo24,
+    logo25,
+    logo26,
+  ];
+
+  const [currentIcon, setCurrentIcon] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Cambia el icono a uno aleatorio
+      setCurrentIcon(Math.floor(Math.random() * icons.length));
+    }, 300); // Tiempo de cambio (ajustable)
+
+    // Limpia el intervalo cuando el componente se desmonte
+    return () => clearInterval(interval);
+  }, [icons.length]);
+
+  return (
+    <div className="loading-overlay">
+      <div className="loading-container">
+        <img
+          src={icons[currentIcon]}
+          alt="Loading icon"
+          className="loading-icon"
+        />
+        <p className="loading-text">Loading...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,37 +1,112 @@
 import { create } from "zustand";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { collection, query, where, getDocs, doc, setDoc } from "firebase/firestore";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  setDoc,
+} from "firebase/firestore";
 import { auth, db } from "../../firebase.config"; // Importa la configuración de Firebase, incluyendo la autenticación y Firestore
+import Swal from "sweetalert2";
+import "../styles/global.css";
 
 // Creación del store de autenticación utilizando Zustand
 const useAuthStore = create((set) => ({
   // Estado inicial
-  user: null,  // Estado para almacenar al usuario autenticado
+  user: null, // Estado para almacenar al usuario autenticado
   error: null, // Estado para almacenar cualquier error que ocurra
 
   // Función para registrar un nuevo usuario
   registerUser: async (email, password, username, navigate) => {
     try {
+      // Verificar si el username ya está en uso en Firestore
+      const q = query(
+        collection(db, "users"),
+        where("username", "==", username)
+      );
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        // Si el nombre de usuario ya está en uso
+        Swal.fire({
+          icon: "error",
+          title: "Ha ocurrido un error",
+          text: "El nombre de usuario ya está registrado. Elige otro.",
+          confirmButtonText: "Entendido",
+          customClass: {
+            confirmButton: "custom-swal-button", // Clase personalizada
+          },
+        });
+        return;
+      }
+
       // Intenta registrar al usuario con email y contraseña
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user = userCredential.user; // Obtenemos el usuario registrado desde Firebase
 
       // Guardar el nombre de usuario y el email en Firestore bajo el ID del usuario
       await setDoc(doc(db, "users", user.uid), {
-        username: username,  // Almacena el nombre de usuario
-        email: email         // Almacena el email
+        username: username, // Almacena el nombre de usuario
+        email: email, // Almacena el email
       });
-      
+
       // Actualiza el estado global con el usuario registrado y limpia los errores
       set({ user: { ...user, username }, error: null }); // Aquí añadimos el username al objeto user
-      console.log("Usuario registrado:", user);
+      // Muestra el mensaje de éxito
+      Swal.fire({
+        icon: "success",
+        title: "Registro exitoso",
+        text: "¡Bienvenido! Tu aventura muisca esta a punto de comenzar.",
+        confirmButtonText: "Vamos a ello!",
+        customClass: {
+          confirmButton: "custom-swal-button", // Clase personalizada
+        },
+      });
 
       // Navega a la página de login tras el registro exitoso
-      navigate('/'); 
+      navigate("/");
     } catch (error) {
-      // Si ocurre un error, lo almacenamos en el estado y lo mostramos en consola
-      set({ error: error.message });
-      console.error("Error al registrar usuario:", error.message);
+      // Si ocurre un error, lo almacenamos en el estado y lo pasamos a validar
+      let message = "Ha ocurrido un error";
+      switch (error.code) {
+        case "auth/email-already-in-use":
+          message = "El correo electrónico ya está en uso";
+          break;
+        case "auth/weak-password":
+          message =
+            "La contraseña es muy débil. Debe tener al menos 8 caracteres";
+          break;
+        case "auth/invalid-email":
+          message = "El formato del correo electrónico es inválido";
+          break;
+        case "auth/operation-not-allowed":
+          message = "El registro de usuarios está deshabilitado temporalmente";
+          break;
+        default:
+          message = error.message;
+      }
+
+      set({ error: message });
+
+      Swal.fire({
+        icon: "error",
+        title: "Error en el registro",
+        text: message,
+        confirmButtonText: "Entendido",
+        customClass: {
+          confirmButton: "custom-swal-button", // Clase personalizada
+        },
+      });
     }
   },
 
@@ -43,32 +118,82 @@ const useAuthStore = create((set) => ({
 
       // Consulta a Firestore para encontrar el usuario con el nombre de usuario proporcionado
       const q = query(usersRef, where("username", "==", username));
-      const querySnapshot = await getDocs(q); // Realiza la consulta
+      const querySnapshot = await getDocs(q);
 
-      // Si no se encuentra ningún usuario con el nombre proporcionado, lanza un error
+      // Si no se encuentra ningún usuario con el nombre proporcionado
       if (querySnapshot.empty) {
-        throw new Error("No se encontró un usuario con ese nombre.");
+        Swal.fire({
+          icon: "error",
+          title: "Usuario no encontrado",
+          text: "No se encontró un usuario con ese nombre.",
+          confirmButtonText: "Aceptar",
+          customClass: {
+            confirmButton: "custom-swal-button", // Clase personalizada
+          },
+        });
+        return;
       }
 
-      // Obtiene el primer documento (usuario) encontrado en la consulta
+      // Obtener el email del usuario encontrado
       const userDoc = querySnapshot.docs[0];
-      const userEmail = userDoc.data().email; // Extrae el email del usuario
-      const userUsername = userDoc.data().username; // Obtener el username del documento
+      const userEmail = userDoc.data().email;
+      const userUsername = userDoc.data().username;
 
-      // Inicia sesión con el email y contraseña del usuario encontrado
-      const userCredential = await signInWithEmailAndPassword(auth, userEmail, password);
-      const user = userCredential.user; // Obtenemos el usuario autenticado
+      // Iniciar sesión con el email y contraseña del usuario encontrado
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        userEmail,
+        password
+      );
+      const user = userCredential.user;
 
-      // Actualiza el estado global con el usuario autenticado, incluyendo el username
-      set({ user: { ...user, username: userUsername }, error: null }); // Aquí añadimos el username al objeto user
-      console.log("Usuario autenticado:", user);
+      // Guardar el usuario autenticado y su username en el estado global
+      set({ user: { ...user, username: userUsername }, error: null });
 
-      // Navega al menú principal tras el inicio de sesión exitoso
-      navigate('/menu'); 
+      // Mensaje de éxito de inicio de sesión
+      Swal.fire({
+        icon: "success",
+        title: "Inicio de sesión exitoso",
+        text: "Choc mhuquy (Bienvenido en muisca), " + username + "!",
+        confirmButtonText: "Aceptar",
+        customClass: {
+          confirmButton: "custom-swal-button", // Clase personalizada
+        },
+      });
+
+      // Navegar al menú principal tras el inicio de sesión exitoso
+      navigate("/menu");
     } catch (error) {
-      // Si ocurre un error, lo almacenamos en el estado y lo mostramos en consola
-      set({ error: error.message });
-      console.error("Error al iniciar sesión:", error.message);
+      let message = "Ha ocurrido un error";
+      switch (error.code) {
+        case "auth/user-not-found":
+          message = "No se encontró un usuario con ese correo electrónico.";
+          break;
+        case "auth/invalid-credential":
+          message = "Contraseña incorrecta.";
+          break;
+        case "auth/too-many-requests":
+          message = "Demasiados intentos. Intenta de nuevo más tarde.";
+          break;
+        case "auth/invalid-email":
+          message = "El correo electrónico es inválido.";
+          break;
+        default:
+          message = error.message;
+      }
+
+      set({ error: message });
+
+      // Mostrar mensaje de error con Swal
+      Swal.fire({
+        icon: "error",
+        title: "Error en el inicio de sesión",
+        text: message,
+        confirmButtonText: "Aceptar",
+        customClass: {
+          confirmButton: "custom-swal-button", // Clase personalizada
+        },
+      });
     }
   },
 
@@ -83,13 +208,13 @@ const useAuthStore = create((set) => ({
       console.log("Usuario ha cerrado sesión.");
 
       // Navega a la página de registro tras cerrar sesión
-      navigate('/registro'); 
+      navigate("/registro");
     } catch (error) {
       // Si ocurre un error, lo almacenamos en el estado y lo mostramos en consola
       set({ error: error.message });
       console.error("Error al cerrar sesión:", error.message);
     }
-  }
+  },
 }));
 
 // Exporta el store para ser utilizado en otros componentes de la aplicación
